ec27254dd22bb21a17b4981a41c110174b8c7130
Elijah Apreko <elijahapreko@gmail.com>
2024-10-13 21:46:23 +0000

    add dummy regions

---
+++ src/pages/dashboard/ghanaRegionx.js
@@ -0,0 +1,13 @@
+const ghanaRegions = [
+  {
+    region: 'Greater Accra',
+    constituencies: ['Accra Central', 'Ablekuma South', 'Tema West']
+  },
+  {
+    region: 'Ashanti',
+    constituencies: ['Kumasi Central', 'Asawase', 'Oforikrom']
+  }
+  // Add more regions and constituencies as needed
+];
+
+export default ghanaRegions;
